9b93bd8ee41eca7079c66a654705ffacd7347558
Torben Koch <torben.koch@clockodo.com>
2025-03-13 15:26:06 +0100

    Log work times and current entry

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { reset } from "./funcs/config";
 import { jira } from "./funcs/jira";
 import path from "path";
 import os from "os";
+import { logRunningEntry, logWorkTimes } from "./utils/workTimes";
 
 enum Mode {
   Jira = "Jira",
@@ -65,6 +66,9 @@ program.action(async () => {
     },
     baseUrl: "http://clockodo.loc/api",
   });
+
+  await logWorkTimes({ clockodo });
+  await logRunningEntry({ clockodo });
 
   const { mode }: { mode: Mode } = await inquirer.prompt([
     {
---
+++ src/utils/workTimes.ts
@@ -0,0 +1,69 @@
+import chalk from "chalk";
+import ora from "ora";
+import { DateTime, Duration } from "luxon";
+import { ClockodoProp } from "../types/clockodo";
+
+export const logWorkTimes = async ({ clockodo }: ClockodoProp) => {
+  const spinner = ora(`Fetching work times...`).start();
+  const startOfToday = DateTime.local({ zone: "Europe/Berlin" })
+    .startOf("day")
+    .startOf("second")
+    .toUTC()
+    .toISO({ suppressMilliseconds: true });
+  const endOfToday = DateTime.local({ zone: "Europe/Berlin" })
+    .endOf("day")
+    .startOf("second")
+    .toUTC()
+    .toISO({ suppressMilliseconds: true });
+
+  const data = await clockodo.getEntries({
+    timeSince: startOfToday,
+    timeUntil: endOfToday,
+  });
+
+  const { running } = await clockodo.getClock();
+  let runningDuration = undefined;
+
+  if (running) {
+    runningDuration = DateTime.fromISO(running.timeSince).diffNow().negate();
+  }
+
+  const totalDuration = data.entries
+    .filter((e) => "duration" in e)
+    .reduce((acc, { duration }) => (acc += duration), 0);
+
+  const duration = Duration.fromMillis(totalDuration * 1000)
+    .plus(runningDuration ?? 0)
+    .toFormat("hh:mm");
+  if (!data.entries) {
+    spinner.fail(chalk.red("No running entry.\n"));
+    return;
+  }
+  spinner.succeed(chalk.green(`You have worked ${duration}h today.\n`));
+};
+
+export const logRunningEntry = async ({ clockodo }: ClockodoProp) => {
+  const spinner = ora(`Fetching running entry...`).start();
+  const { running } = await clockodo.getClock();
+
+  if (!running) {
+    spinner.info("Welcome to Clockodo! You are currently not tracking time.\n");
+    return;
+  }
+
+  const { customer } = await clockodo.getCustomer({ id: running.customersId });
+
+  const duration = DateTime.fromISO(running.timeSince)
+    .diffNow()
+    .negate()
+    .toFormat("hh:mm");
+
+  spinner.succeed(chalk.green(`Welcome to Clockodo!`));
+  console.info(
+    chalk.magenta(
+      `You are currently tracking ${
+        running.text ? `'${running.text}'` : "without description"
+      } on ${customer.name} for ${duration}h.\n`
+    )
+  );
+};
