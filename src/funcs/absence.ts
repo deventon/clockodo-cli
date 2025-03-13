6cbedd8d1151e80b85767f22c9357640c606ad12
Torben Koch <torben.koch@clockodo.com>
2025-03-13 17:27:14 +0100

    Refactor absence prompts

--- src/funcs/absence.ts
+++ src/funcs/absence.ts
@@ -2,13 +2,14 @@ import { ClockodoProp } from "../types/clockodo";
 import inquirer from "inquirer";
 import { DateTime } from "luxon";
 import { AbsenceType } from "clockodo";
+import { confirm, input } from "@inquirer/prompts";
 
 export const absence = async ({ clockodo }: ClockodoProp) => {
   const today = DateTime.now().toISODate();
 
-  let sick_note = undefined;
-  let count_hours = undefined;
-  let half_day = undefined;
+  let sickNote = undefined;
+  let countHours = undefined;
+  let halfDay = undefined;
 
   const { type }: { type: AbsenceType } = await inquirer.prompt([
     {
@@ -44,48 +45,36 @@ export const absence = async ({ clockodo }: ClockodoProp) => {
   ]);
 
   if (type === AbsenceType.SickDay || type === AbsenceType.SickDayOfChild) {
-    const { hasSickNote } = await inquirer.prompt([
-      {
-        type: "confirm",
-        name: "hasSickNote",
-        default: false,
-        message: "Do you have a sick note?",
-      },
-    ]);
-    sick_note = Number(hasSickNote);
+    const hasSickNote = await confirm({
+      default: false,
+      message: "Do you have a sick note?",
+    });
+
+    sickNote = Number(hasSickNote);
   }
 
   if (type === AbsenceType.ReductionOfOvertime) {
-    const { countHours } = await inquirer.prompt([
-      {
-        type: "input",
-        name: "countHours",
-        default: "0",
-        message: "Enter the amount of hours",
-      },
-    ]);
-    count_hours = countHours;
+    countHours = await input({
+      default: "0",
+      message: "Enter the amount of hours",
+    });
   }
 
   if (type !== AbsenceType.ReductionOfOvertime && dateSince === dateUntil) {
-    const { halfDay } = await inquirer.prompt([
-      {
-        type: "confirm",
-        name: "halfDay",
-        default: false,
-        message: "Add this as a half day absence?",
-      },
-    ]);
+    const isHalfDay = await confirm({
+      default: false,
+      message: "Add this as a half day absence?",
+    });
 
-    half_day = Number(halfDay);
+    halfDay = Number(isHalfDay);
   }
 
   await clockodo.addAbsence({
     type,
     dateSince,
     dateUntil,
-    half_day,
-    sick_note,
-    count_hours,
+    halfDay,
+    sickNote,
+    countHours,
   });
 };
