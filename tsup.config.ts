4a046a7eaf8a35e29f2cc54cae1947c24adeb0c4
Torben Koch <torben.koch@clockodo.com>
2025-03-13 10:49:23 +0100

    Add .env

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsup";
+import dotenv from "dotenv";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  env: dotenv.config().parsed,
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
